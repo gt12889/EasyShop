e1239efddb4fa0175a1cbf18c92decbef60299ba
Gt ---- <t.dinh43204@gmail.com>
2023-06-27 02:03:03 +0700

    put

--- easyshop-api/src/main/java/org/yearup/controllers/CategoriesController.java
+++ easyshop-api/src/main/java/org/yearup/controllers/CategoriesController.java
@@ -2,18 +2,18 @@ package org.yearup.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import org.yearup.data.CategoryDao;
 import org.yearup.data.ProductDao;
+import org.yearup.data.mysql.MySqlCategoryDao;
 import org.yearup.models.Category;
 import org.yearup.models.Product;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 // add the annotations to make this a REST controller
 // add the annotation to make this controller the endpoint for the following url
@@ -68,14 +68,29 @@ public class CategoriesController
     public Category addCategory(@RequestBody Category category)
     {
         // insert the category
+
         return null;
     }
 
     // add annotation to call this method for a PUT (update) action - the url path must include the categoryId
     // add annotation to ensure that only an ADMIN can call this function
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PutMapping(path = "categories/{id}")
     public void updateCategory(@PathVariable int id, @RequestBody Category category)
     {
         // update the category by id
+        var currentCategory =  categoryDao.getById(id);
+
+        if(currentCategory == null)
+        {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "That product does not exist.");
+        }
+        try
+        {
+            categoryDao.update(id, category);
+        }
+        catch(Exception ex)
+        {}
     }
 
 
